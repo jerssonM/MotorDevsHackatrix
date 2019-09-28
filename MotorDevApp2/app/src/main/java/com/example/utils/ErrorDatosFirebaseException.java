af03a5bac98fcc566c3000fe1a2b5dd38a953b15
Jorge Andres Garcia Bernal <andres.garcia@infotrack.com.co>
2019-09-28 17:38:11 -0500

    checkIn app

---
+++ MotorDevApp2/app/src/main/java/com/example/utils/ErrorDatosFirebaseException.java
@@ -0,0 +1,24 @@
+package com.example.utils;
+
+import com.google.firebase.database.DatabaseError;
+
+import io.reactivex.annotations.NonNull;
+
+public class ErrorDatosFirebaseException extends Exception {
+    protected DatabaseError error;
+
+    public ErrorDatosFirebaseException(@NonNull DatabaseError error) {
+        this.error = error;
+    }
+
+    public DatabaseError getError() {
+        return error;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorDatosFirebaseException{" +
+                "error=" + error +
+                '}';
+    }
+}
